5d4419882a105100ae23f6bd672af93e4b668c5c
sebastianhernandezo-prog <sebastian.hernandezo@autonoma.edu.co>
2026-04-25 16:31:06 -0500

    Nombre del modificador del codigo: Sebastian Hernandez Osorio
    Corregui varios errores que habian en el codigo de Vuelos, ademas de implementar el atributo servicios donde se pueden agregar algun servicio que ofrese el Vuelo, tambien le implemente 2 metodos donde puedes modificar la hora del vuelo y ademas un metodo para modificar el servicio que ofrece el vuelo, ademas de que le agrege un main para poder corroborar el codigo.

--- Vuelo.java
+++ Vuelo.java
@@ -4,47 +4,58 @@ public class Vuelo {
     public int telefono;
     public int hora;
     public String ubicacion;
-    public Vuelo(String nombre, int telefono, int hora, String ubicacion) {
+    public String servicios;
+
+    public Vuelo(String nombre, int telefono, int hora, String ubicacion, String servicios) {
         this.nombre = nombre;
         this.telefono = telefono;
         this.hora = hora;
         this.ubicacion = ubicacion;
+        this.servicios = servicios;
     }
 
-   
     public String getNombre() { 
         return nombre; 
     }
+
     public int getTelefono() { 
         return telefono; 
     }
+
     public int getHora() { 
         return hora; 
     }
+
     public String getUbicacion() { 
         return ubicacion; 
     }
+
     public void mostrarInfo() {
-        System.out.println("Vuelo: " + nombre);
-        System.out.println("Teléfono: " + telefono);
+        System.out.println("--- DETALLE COMPLETO ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Ubicacion: " + ubicacion);
         System.out.println("Hora: " + hora);
-        System.out.println("Ubicación: " + ubicacion);
+        System.out.println("Telefono: " + telefono);
+        System.out.println("Servicios: " + servicios);
     }
 
-    public void mostrarInfo(boolean detallado) {
-        if (detallado) {
-            System.out.println("--- DETALLE COMPLETO ---");
-            System.out.println("Nombre: " + nombre);
-            System.out.println("Ubicacion: " + ubicacion);
-            System.out.println("Hora: " + hora);
-            System.out.println("Telefono: " + telefono);
-        } else {
-            mostrarInfo();
-        }
+    public void cambiarhora(int nueva_hora){
+        this.hora = nueva_hora;
     }
 
+    public void modificarservicio(String agregar_servicio){
+        this.servicios = agregar_servicio;
+    }
 
     public void mostrarInfo(String mensajePersonalizado) {
         System.out.println(mensajePersonalizado + " " + nombre);
+    }
+
+    public static void main(String[] args) {
+        Vuelo vuelo = new Vuelo("HPMYADMIN", 320200342, 10, "medellin", "Se ofrece un incrible bufet que va incluido en la compra del vuelo");
+        
+        vuelo.cambiarhora(10);
+        vuelo.mostrarInfo();
+        vuelo.modificarservicio("Se ofrece un asiento exclusivo para todos los que an estado en nuestra aerolinea durante mas de 10 años");
     }
 }
